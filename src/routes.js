7afedb73a3cdeb551a55246cebcb61dc1ad1b27f
jacoborbach <jacoborbach96@gmail.com>
2021-01-12 15:54:53 -0500

    second push

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import ClassList from './components/ClassList/ClassList';
 
 export default (
     <Switch>
         <Route exact path='/' component={Home}></Route>
         <Route path='/about' component={About}></Route>
+        <Route path='/classlist/:class' component={ClassList}></Route>
     </Switch>
 )
