3c4dd8f92c4d69e145118031785f44c0243b62a9
jacoborbach <jacoborbach96@gmail.com>
2021-01-12 16:17:17 -0500

    third push

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => this.setState({ students: res.data }))
+      .catch(err => console.log(err))
   }
 
   render() {
+    // console.log(this.props)
+    const mappedStudents =
+      this.state.students.map(element => {
+        return (
+          <h3 key={element.id}>{element.first_name} {element.last_name}</h3>
+        )
+      })
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
       </div>
     )
   }
